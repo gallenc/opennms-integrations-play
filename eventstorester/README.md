357ede49652de8bf8753e8c643e701d7206d45b4
gallenc <cgallen@opennms.org>
2022-11-28 20:36:10 +0000

    working simple rule

---
+++ eventstorester/README.md
@@ -0,0 +1,4 @@
+# notes
+
+postman collection parsing libary - probably to complex 
+https://github.com/poynt/postman-runner
---
+++ eventstorester/src/test/java/org/opennms/integertion/eventtorestor/JsonIT.java
@@ -0,0 +1,14 @@
+package org.opennms.integertion.eventtorestor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JsonIT {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
